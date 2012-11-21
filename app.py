1809f3b774ce4de6581b0a578143d6ed89ae58c5
maldra <meagan.bufgell@gmail.com>
2012-11-21 02:54:35 -0500

    Hacked app.py til it worked. The DB works!

--- app.py
+++ app.py
@@ -7,9 +7,16 @@ import sys, os
 connection = connectionForURI("mysql://root:scr33m0@localhost/SEG2105")
 sqlhub.processConnection= connection
 sqlhub.processConnection.debug = True; sqlhub.processConnection.debugOutput=True
-#Creating DB Tables if DNE
-for i in (Notification, Employee, Request, TimeOff, Shift, Schedule, Store):
-	i.createTable(ifNotExists=True)
+
+#Creating DB Tables if DNE, runs multiple times to fix ordering.
+done = False
+while not done:
+	try:
+		for i in (Employee, Notification, Request, TimeOff, Shift, Schedule, Store):
+			i.createTable(ifNotExists=True)
+		done = True
+	except:
+		pass
 
 #Setting up Devel Server
 app=Bottle()
