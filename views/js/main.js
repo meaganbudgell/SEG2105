4c218017c8a6a8fabbcbd5a573fff06286a65dbf
maldra <meagan.bufgell@gmail.com>
2012-11-25 05:21:57 -0500

    Added Task, login and logout button functionality.

--- views/js/main.js
+++ views/js/main.js
@@ -1,1 +1,41 @@
+$(document).ready(function(){
+//Buttons in order of appearance
 
+	$('#loginButton').click(function(){
+		$('#loginPanel').hide();
+		$('#mainContentPanel').show();
+		$('#schedulePanel').show();
+	});
+	$('#logoutBox').click(function(){
+		$('#loginPanel').show();
+		$('#mainContentPanel').hide();
+		$('#schedulePanel').hide();
+	});
+	$('#backToMain').click(function(){
+		//No point in if statements, no matter what we want all hidden except one.
+		$('#employeeManagementPanel').hide();
+		$('#shiftManagementPanel').hide();	
+		$('#deadlinePanel').hide();
+		$('#schedulePanel').show();
+	});
+	$('#employeeManagementButton').click(function(){
+		$('#employeeManagementPanel').show();
+		$('#shiftManagementPanel').hide();	
+		$('#deadlinePanel').hide();
+		$('#schedulePanel').hide();
+	});
+	$('#shiftManagementButton').click(function(){
+		$('#employeeManagementPanel').hide();
+		$('#shiftManagementPanel').show();	
+		$('#deadlinePanel').hide();
+		$('#schedulePanel').hide();
+	});
+	$('#deadlineManagementButton').click(function(){
+		$('#employeeManagementPanel').hide();
+		$('#shiftManagementPanel').hide();	
+		$('#deadlinePanel').show();
+		$('#schedulePanel').hide();
+	});
+	
+
+});
