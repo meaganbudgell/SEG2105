61a05cb31ab65d380c2c977087045a0d1fd2281a
maldra <meagan.bufgell@gmail.com>
2012-11-26 18:06:40 -0500

    Testing out dictionary function

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from bottle import Bottle, run, template
 from controller import *
 from sqlobject import *
-from pickle import *
+from json import *
+import jsonpickle
+import json
 import sys, os
 
 
@@ -31,7 +33,36 @@ def hello():
 @app.get("/testConstructor")
 def get():
 	jane=Employee(name="jane",isManager=False,login="5464")	
-	
-	return jane.__dict__
+	def to_dict(obj):
+		# each type of column
+		foreigns = obj.sqlmeta._plainForeignGetters.keys()
+		joins = obj.sqlmeta.joinDefinitions
+
+		# all of the basic stuff is built-in (as I recently discovered -_- )
+		d = obj.sqlmeta.asDict()
+
+		# convert all datetimes/dates/times to something JSON can use
+		for k in d:
+		       if isinstance(d[k], datetime.datetime) or isinstance(d[k], datetime.date) or isinstance(d[k], datetime.time):
+			       d[k] = d[k].isoformat()
+
+		# function given class and list of objects of that class, returns object dict for that reference
+		# an object dict is a dictionary with 2 keys.  name is the name of the class that the objects contained are instances of
+		# items is the list of IDs referenced.
+		def f(klass, objs):
+		       return dict(name=klass,
+				   items=[x.id for x in objs])
+
+		# convert all foreign keys to one-element object dicts
+		for key in foreigns:
+		       d.update({key:f(getattr(obj, key)._class_._name_, [getattr(obj, key)])})
+
+		# convert all joins to object dicts
+		for key in joins:
+		       d.update({key.name:f(key.kw['otherClass'], getattr(obj, key.name))})
+       
+       		return d
+	return json.dump(to_dict(jane))
+
 run (app,host="localhost", port=8080)
 
