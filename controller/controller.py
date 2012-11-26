1a2ab932b00f696cfa0862909ff69c30183ab3c1
maldra <meagan.bufgell@gmail.com>
2012-11-26 18:46:19 -0500

    Fixed Controller

--- controller/controller.py
+++ controller/controller.py
@@ -1,28 +1,28 @@
 from model import *
 
 def checkLogin(name, login):
-
+	pass
 
 def getMonthDetails(date):
-
+	pass
 
 def loadSchedule(employee_id):
-
+	pass
 
 def checkSchedule(date):
-
+	pass
 
 def loadNotifications(name):
-
+	pass
 
 def loadRequests(name):
-
+	pass
 
 def answerRequest(isApproved):
-
+	pass
 
 def addShiftBlock(startTime, endTime):
-
+	pass
 
 def addEmployee(name, isManager, loginPassword, store_id):
 	# Creates a new employee
@@ -33,23 +33,23 @@ def addEmployee(name, isManager, loginPassword, store_id):
 	e.stores.insert(store_id)
 
 def loadEmployeeList(storeName):
-
+	pass
 
 def loadStoreList:
-
+	pass
 
 def loadTemplateShifts:
-
+	pass
 
 def removeShiftBlock(shift_id):
-
+	pass
 
 def addEmployeeToShift(employee, shift_id):
 	# Adds the given employee to the given shift
 	shift_id.employee = employee
 
 def checkAvailableEmployees(shift_id):
-
+	pass
 
 def addShiftToDay(shift_id, day_id):
 	# Adds the given shift to the given day
@@ -60,22 +60,22 @@ def removeShiftFromDay(shift_id, day_id):
 	day_id.removeShift(shift_id)
 
 def loadDay(day_id):
-
+	pass
 
 def addUnavailableDay(day_id):
-
+	pass
 
 def removeUnavailableDay(day_id):
-
+	pass
 
 def getScheduleDeadline:
-
+	pass
 
 def setScheduleDeadline(date):
-
+	pass
 
 def addTimeOffRequest(employee_id, date):
-
+	pass
 
 def viewNotification(isSeen):
-
+	pass
