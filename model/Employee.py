a61b9ea5c4a35788e65e2091ce7fcd4e672547c5
Jordan Bussanich <jordan.bussanich@gmail.com>
2012-11-26 22:30:58 -0500

    Fixed _id_id.

--- model/Employee.py
+++ model/Employee.py
@@ -8,7 +8,7 @@ class Employee(SQLObject):
 	notifications = MultipleJoin('Notification', joinColumn="receiver_id")
 	unavailableDays = MultipleJoin('UnavailableDay')
 	stores = RelatedJoin('Store')
-	requests = MultipleJoin('Request', joinColumn="sender_id_id")
+	requests = MultipleJoin('Request', joinColumn="sender_id")
 	shifts = MultipleJoin('Shift')
 	login = StringCol()
 	
