85f305fc5339a9ff6f157788278a536e7287800d
maldra <meagan.budgell@gmail.com>
2012-11-29 03:06:27 -0500

    Fixed.params to .json and fixed function headings

--- controller/controller.py
+++ controller/controller.py
@@ -7,60 +7,60 @@ import default_app
 app=default_app()
 
 @app.post("/addDayToShift/")
-def addDayToShift(shift_id, day):
-	shift_id=request.params[0]
-	day=request.params[1]
+def addDayToShift():
+	shift_id=request.json.shift_id
+	day=request.json.day
 	shift=Shift.select(Shift.q.id==shift_id)[0]
 	shift.addDay(day)
 
 @app.post("/addEmployeeToShift/")
-def addEmployeeToShift(shift_id, eName):
-	shift_id=request.params[0]
-	eName=request.params[1]
+def addEmployeeToShift():
+	shift_id=request.json.shift_id
+	eName=request.json.eName
 	employee=Employee.select(Employee.q.name==eName)[0]
 	shift=Shift.select(Shift.q.id==shift_id)[0]
 	shift.addEmployee(employee)
 
 @app.post("/addEmployeeToStore/")
-def addEmployeeToStore(sName, eName):
-	sName=request.params[0]
-	eName=request.params[1]
+def addEmployeeToStore():
+	sName=request.json.sName
+	eName=request.json.eName
 	store = Store.select(Store.q.name == sName)[0]
 	employee=Employee.select(Employee.q.name==eName)[0]
 	store.addEmployee(employee.id)
 
 @app.post("/addTemplateShift/")
-def addTemplateShift(inputStartTime, inputEndTime):
-	inputStartTime=request.params[0]
-	inputEndTime=request.params[1]
+def addTemplateShift():
+	inputStartTime=request.json.inputStartTime
+	inputEndTime=request.json.inputEndTime
 	s = Shift(startTime = inputStartTime, endTime = inputEndTime, employee = None, day=None)
 
 @app.post("/addTimeOffRequest/")
-def addTimeOffRequest(eName, date):
-	eName=request.params[0]
-	date=request.params[1]
+def addTimeOffRequest():
+	eName=request.json.eName
+	date=request.json.date
 	employee = Employee.select(Employee.q.name==eName)[0]
 	r = TimeOff(day = date.timetuple().tm_yday, sender = employee.id)
 
 @app.post("/addUnavailableDay/")
-def addUnavailableDay(day, eName):
-	day=request.params[0]
-	eName=request.params[1]
+def addUnavailableDay():
+	day=request.json.day
+	eName=request.json.eName
 	employeeID= Employee.select(Employee.q.name==employeeName)[0]
 	ud = UnavailableDay(day = day_id, employee=employeeID.id)
 
 
 @app.post("/answerRequest/")
-def answerRequest(request_id, answer):
-	request_id=request.params[0]
-	answer=request.params[1]
+def answerRequest():
+	request_id=request.json.request_id
+	answer=request.json.answer
 	request= Request.select(Request.q.id==request_id)[0]
 	request.answer(answer)
 
 
 @app.get("/checkAvailableEmployees/")
-def checkAvailableEmployees(shift_id):
-	shift_id=request.params[0]	
+def checkAvailableEmployees():
+	shift_id=request.json.shift_id	
 	s = Shift.select(Shift.q.id == shift_id)[0]
 	d = s.day
 	available = []
@@ -84,16 +84,16 @@ def checkDaySchedule(day, store):
 		return None
 
 @app.get("/checkEmployeeLogin/")
-def checkEmployeeLogin(eName, loginCode):
-	eName=request.params[0]
-	loginCode=request.params[1]
+def checkEmployeeLogin():
+	eName=request.json.eName
+	loginCode=request.json.loginCode
 	employee= Employee.select(Employee.q.name==employeeName)[0]
 	return employee.checkLogin(loginCode)
 
 @app.get("/checkSchedule/")
-def checkSchedule(date,storeName):
-	date=request.params[0]
-	storeName=request.params[1]
+def checkSchedule():
+	date=request.json.date
+	sName=request.json.sName
 	pyDate=datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%fZ')
 	monthName = pyDate.strftime('%B')
 	firstOfMonth = pyDate.replace(day=1)
@@ -103,17 +103,18 @@ def checkSchedule(date,storeName):
 	firstOfMonthWeekDay = int(firstOfMonth.strftime('%w'))
 
 @app.post("/fireEmployee/")
-def fireEmployee(eName):
-	eName=request.params[0]
+def fireEmployee():
+	eName=request.json.eName
 	Employee.delete(Employee.q.name == eName)
 
 @app.post("/hireEmployee/")
-def hireEmployee(eName, isManager, loginPassword, sName):
-	eName=request.params[0]
-	isManager=request.params[1]
-	loginPassword=request.params[2]
-	sName=request.params[3]
-	e = Employee(name=ename, isManager=isManager, login=loginPassword)
+def hireEmployee():
+	eName=request.json.eName
+	isManager=request.json.isManager
+	loginCode=request.json.loginCode
+	sName=request.json.sName
+
+	e = Employee(name=ename, isManager=isManager, login=loginCode)
 	if (storeName==None):
 		for store in Store.select():
 			store.addEmployee(e)
@@ -122,24 +123,25 @@ def hireEmployee(eName, isManager, loginPassword, sName):
 		store.addEmployee(e)
 
 @app.get("/loadNotifications/")
-def loadNotifications(eName):
-	eName=request.params[0]
+def loadNotifications():
+
+	eName=request.json.eName
 	employee = Employee.select(Employee.q.name == employeeName)
 	result = dict()
 	result["items"] = [x.dict for x in employee.notifications]
 	return result	
 
 @app.get("/loadRequests/")
-def loadRequests(eName):
-	eName=request.params[0]
+def loadRequests():
+	eName=request.json.eName
 	employee = Employee.select(Employee.q.name == eName)
 	result = dict()
 	result["items"] = [x.dict for x in employee.requests]
 	return result
 
 @app.get("/loadStoreEmployees/")
-def loadStoreEmployees(sName):
-	sName=request.params[0]
+def loadStoreEmployees():
+	sName=request.json.sName
 	store = Store.select(Store.q.name == sName)[0]
 	result = dict()
 	result["items"]=[x.dict for x in store.employees]
@@ -164,50 +166,50 @@ def loadTemplateShifts():
 	return result
 
 @app.post("/removeDayFromShift/")
-def removeDayFromShift(shift_id, day):
-	shift_id=request.params[0]	
-	day=request.params[1]
+def removeDayFromShift():
+	shift_id=request.json.shift_id	
+	day=request.json.day
 	shift=Shift.select(Shift.q.id==shift_id)[0]
 	shift.removeDay(day)
 
 @app.post("/removeEmployeeFromShift/")
-def removeEmployeeFromShift(shift_id, eName):
-	shift_id=request.params[0]
-	eName=request.params[1]
+def removeEmployeeFromShift():
+	shift_id=request.json.shift_id
+	eName=request.json.eName
 	employee=Employee.select(Employee.q.name==eName)[0]
 	shift=Shift.select(Shift.q.id==shift_id)[0]
 	shift.removeEmployee(employee)
 
 @app.post("/remobeEmployeeFromStore/")
-def removeEmployeeFromStore(sName, eName):
-	sName=request.params[0]
-	eName=request.params[1]
+def removeEmployeeFromStore():
+	sName=request.json.sName
+	eName=request.json.eName
 	store = Store.select(Store.q.name == sName)[0]
 	employee=Employee.select(Employee.q.name==eName)[0]
 	store.removeEmployee(employee.id)
 
 @app.post("/removeTemplateShift/")
-def removeTemplateShift(shift_id):
-	shift_id=request.params[0]
+def removeTemplateShift():
+	shift_id=request.json.shift_id
 	shift = Shift.select(Shift.q.id == shift_id, Shift.q.day==None)[0]
 	shift.destroySelf()
 
 @app.post("/removeUnavailableDay/")
-def removeUnavailableDay(day_id, eName):
-	day_id=request.params[0]
-	eName=request.params[1]
+def removeUnavailableDay():
+	day_id=request.json.day_id
+	eName=request.json.eName
 	employeeID= Employee.select(Employee.q.name==eName)[0]
 	ud = UnavailableDay.select(UnavailableDay.q.day == day_id, UnavailableDay.q.employee==employeeID.id)[0]
 	ud.destroySelf()
 
 @app.post("/setScheduleDeadline/")
-def setScheduleDeadline(date):
-	date=request.params[0]
+def setScheduleDeadline():
+	date=request.json.date
 	Store.deadline["deadline"] = date
 
 @app.post("/viewNotification/")
-def viewNotification (notification_id):
-	notification_id=request.params[0]
+def viewNotification ():
+	notification_id=request.json.notification_id
 	notification= Notification.select(Notification.q.id==notification_id)[0]
 
 
